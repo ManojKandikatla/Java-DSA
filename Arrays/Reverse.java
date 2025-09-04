4ccdf25bc337c4a50f079265bc9cee1a13491247
ManojKandikatla <kandikatlamanojkumar58681@gmailcom>
2025-09-04 21:35:01 +0530

    added code

--- Arrays/Reverse.java
+++ Arrays/Reverse.java
@@ -1,10 +1,31 @@
+import java.util.Arrays;
 public class Reverse{
     public static void main(String[] args){
-        String s= "raraaaa";
-        String ans ="";
-        for(int i=s.length()-1;i>=0;i--){
-            ans =ans + s.charAt(i);
+        // String s= "raraaaa";
+        // String ans ="";
+        // for(int i=s.length()-1;i>=0;i--){
+        //     ans =ans + s.charAt(i);
+        // }
+        // System.out.print(ans);
+
+        int[] arr = {1,2,23,8,43};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void reverse(int[] arr){
+        int start =0;
+        int end = arr.length-1;
+
+        while (start<end) {
+            //swap
+            swap(arr, start, end);
+            start++;
+            end--;
         }
-        System.out.print(ans);
+    }
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
