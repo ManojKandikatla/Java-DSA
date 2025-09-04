238f2bafd36d119fb4f0db856e096e934d3f7ab0
ManojKandikatla <kandikatlamanojkumar58681@gmailcom>
2025-09-04 21:21:25 +0530

    added comments

--- ArrayList/ArrayListEX.java
+++ ArrayList/ArrayListEX.java
@@ -4,7 +4,7 @@ public class ArrayListEX{
          ArrayList <Integer> arr = new ArrayList<>();
          ArrayList <Integer> arr2 = new ArrayList<>();
 
-        ArrayList<ArrayList <Integer>> temp = new ArrayList<>();
+        ArrayList<ArrayList <Integer>> temp = new ArrayList<>(6);//it can store more then its capacity because the logic is when ever the array is half full it will dobuble the size of the array and stores the prev values in the doubled array so that we can store n values still the capacity is mentioned
 
         arr.add(5);
         arr.add(51);
