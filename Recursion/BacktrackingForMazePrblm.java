2b81e23e8ad55fdd01368dbc07410454b1eecff0
ManojKandikatla <kandikatlamanojkumar58681@gmailcom>
2025-09-14 23:21:43 +0530

    first commit

--- Recursion/BacktrackingForMazePrblm.java
+++ Recursion/BacktrackingForMazePrblm.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class BacktrackingForMazePrblm {
     
     public static void main(String[] args) {
@@ -9,7 +10,11 @@ public class BacktrackingForMazePrblm {
             {true,true,true},//false is an obstical in [3][3] borad
             {true,true,true},
         };
+
         allPath("", board, 0, 0);
+        int[][] path = new int[board.length][board[0].length];
+
+        allPathPrint("", board, 0, 0, path, 1);
     }
 
     static void allPath(String p, boolean[][] maze, int r, int c) {
@@ -50,6 +55,51 @@ public class BacktrackingForMazePrblm {
         // Backtracking: Unmark cell as visited for other paths
         maze[r][c] = true;
     }
+
+
+    static void allPathPrint(String p, boolean[][] maze, int r, int c,int[][] path, int step) {
+        // Base case: reached destination
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+            path[r][c] = step;
+            for (int[] arr:path) {
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        // If cell is blocked or already visited
+        if (!maze[r][c]) {
+            return;
+        }
+
+        // Mark cell as visited
+        maze[r][c] = false;
+        path[r][c] = step;
+
+        // Move Down
+        if (r < maze.length - 1) {
+            allPathPrint(p + 'D', maze, r + 1, c,path,step+1);
+        }
+
+        // Move Right
+        if (c < maze[0].length - 1) {
+            allPathPrint(p + 'R', maze, r, c + 1,path,step+1);
+        }
+
+        // Move Up
+        if (r > 0) {
+            allPathPrint(p + 'U', maze, r - 1, c,path,step+1);
+        }
+
+        // Move Left
+        if (c > 0) {
+            allPathPrint(p + 'L', maze, r, c - 1,path,step+1);
+        }
+
+        // Backtracking: Unmark cell as visited for other paths
+        maze[r][c] = true;
+         path[r][c] = 0;
+    }
 }
-
-
